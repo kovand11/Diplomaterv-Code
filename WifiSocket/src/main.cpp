cce4c42823e0886f03788083496f9c960a3d1deb
András Kovács <kovand11@gmail.com>
2017-05-05 01:50:44 +0200

    WifiSocket relay switching testprogram

--- WifiSocket/src/main.cpp
+++ WifiSocket/src/main.cpp
@@ -1,11 +1,24 @@
 #include<Arduino.h>
 
+#define RELAY 0
+
+bool relayOn = false;
+
 void setup()
 {
+  pinMode(RELAY, OUTPUT);
+  digitalWrite(RELAY, LOW);
 
 }
 
 void loop()
 {
+  delay(5000);
+  if (relayOn)
+    digitalWrite(RELAY, HIGH);
+  else
+    digitalWrite(RELAY, LOW);
+  relayOn = !relayOn;
+
 
 }
